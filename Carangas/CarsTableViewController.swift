361a250773ffb189cd9a7b4aa6e473d9f2c34995
Usuário Convidado <Guest@pa06micro05.local>
2019-03-13 21:04:19 -0300

    metodos get api carangas

--- Carangas/CarsTableViewController.swift
+++ Carangas/CarsTableViewController.swift
@@ -8,23 +8,36 @@
 import UIKit
 
 class CarsTableViewController: UITableViewController {
-
+    var cars: [Car] = []
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        //[weak self] referencia freca para nao deixa na memoria
+        REST.loadCars { [weak self] (loadeCars) in
+           // guard let self = self else {return}  so para exibir
+            self?.cars = loadeCars
+            //para execultar na main theard  que e a theard principal para elementos visuais 
+            DispatchQueue.main.async {
+                 self?.tableView.reloadData()
+            }
+           
+        }
     }
     
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return cars.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let car  = cars[indexPath.row]
+        cell.textLabel?.text = car.name
+        cell.detailTextLabel?.text = car.brand
         return cell
     }
 }
--- Carangas/REST.swift
+++ Carangas/REST.swift
@@ -14,11 +14,52 @@ class REST {
     
     private static let configuration : URLSessionConfiguration = {
        let config = URLSessionConfiguration.default
-        
-        
+           config.allowsCellularAccess = false
+           config.httpAdditionalHeaders = ["Content-type":"applicatioj/json"]
+           config.timeoutIntervalForRequest = 30.0
+           config.httpMaximumConnectionsPerHost = 3
        return config
     }()
     private static let session = URLSession(configuration: configuration)
+    
+    class func loadCars(onComplete:@escaping ([Car]) -> Void){
+        
+        guard let url = URL(string: basePath) else {
+            print("Erro ao montar URL")
+            return
+        }
+        let task  = session.dataTask(with: url) { (data, response , error) in
+            
+            if error != nil {
+                print("Deus erro", error!)
+                return
+            }
+            guard let response = response as?  HTTPURLResponse  else{
+                print("REsponde nulo")
+                return
+            }
+            if  response.statusCode != 200 {
+                print("Deu Ruim",response.statusCode)
+                return
+            }
+            guard let data = data else {
+                print("Dados inválidos")
+                return
+            }
+            
+            do{
+                let cars = try JSONDecoder().decode([Car].self, from: data)
+                onComplete(cars)
+                print("total de carro", cars.count)
+            }catch{
+                print(error)
+            }
+            
+        }
+        task.resume()
+        
+        
+    }
     
     
     
